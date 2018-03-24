803bfe2632129865420f4f7406e7b5bc3da7ddbd
kevinfang2 <kevkev0123@gmail.com>
2018-03-24 16:06:41 -0700

    tokenize thing

--- array.js
+++ array.js
@@ -1,6 +1,56 @@
-var rules = require('./rules.js')
+var express = require('express')
 var tokenizer = require('tokenizer-array')
+var bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-module.exports = function (src) {
-  return tokenizer(src, rules)
+
+
+app.get('/', function(req,res){
+})
+
+app.post('/interpret', function(req, res){
+  var src = req.body.src
+  var tokenized = tokenize(src)
+  console.log(tokenized)
+  res.send(tokenized)
+})
+
+app.listen(8000)
+console.log('8000')
+
+
+var rules = [
+  { regex: /^\/\*([^*]|\*(?!\/))*\*\/$/, type: 'area comment' },
+  { regex: /^\/\*([^*]|\*(?!\/))*\*?$/, type: 'area comment continue' },
+  { regex: /^\/\/[^\n]*$/, type: 'line comment' },
+  { regex: /^"([^"\n]|\\")*"?$/, type: 'quote' },
+  { regex: /^'(\\?[^'\n]|\\')'?$/, type: 'char' },
+  { regex: /^'[^']*$/, type: 'char continue' },
+  { regex: /^#(\S*)$/, type: 'directive' },
+  { regex: /^\($/, type: 'open paren' },
+  { regex: /^\)$/, type: 'close paren' },
+  { regex: /^\[$/, type: 'open square' },
+  { regex: /^\]$/, type: 'close square' },
+  { regex: /^{$/, type: 'open curly' },
+  { regex: /^}$/, type: 'close curly' },
+  { regex: /^([-<>~!%^&*\/+=?|.,:;]|->|<<|>>|\*\*|\|\||&&|--|\+\+|[-+*|&%\/=]=)$/,
+      type: 'operator' },
+  { regex: /^([_A-Za-z]\w*)$/, type: 'identifier' },
+  { regex: /^[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/, type: 'number' },
+  { regex: /^(\s+)$/, type: 'whitespace' },
+  { regex: /^\\\n?$/, type: 'line continue' }
+]
+
+function tokenize(src){
+  var tokens = tokenizer(src, rules)
+  var realtokens = []
+
+  tokens.forEach(function (token) {
+    realtokens.push(JSON.stringify(token))
+  })
+  return realtokens
 }
---
+++ scraper.py
@@ -0,0 +1,4 @@
+https://www.arduino.cc/reference/en/
+
+library_links = "https://www.arduino.cc/en/Reference/Libraries"
+default_link = "https://www.arduino.cc/reference/en/"
