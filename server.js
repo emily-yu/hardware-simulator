7daecf09dab6310ed48f658e55357cf4b2e45789
kevinfang2 <kevkev0123@gmail.com>
2018-03-24 15:36:47 -0700

    im a dumb ass

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
+//ignore this whole fucking thing and make it on front end
 var request = require('request');
+// const app = express()
 
 var client = process.env.jdoodle_client
 var secret = process.env.jdoodle_secret
 
-var postData = {
-  script:'printf(\"hello world\")',
-  language:'c',
-  versionIndex:'0',
-  clientId:client,
-  clientSecret:secret
-}
-console.log(JSON.stringify({postData}))
-
-var options = {
-  method:'post',
-  url: 'https://api.jdoodle.com/v1/execute',
-  headers: {
-    'Content-Type': 'application/json',
-    'charset' : 'UTF-8'
-  },
-  body: JSON.stringify(postData)
-};
 
 
-request(options, function (err, res, body) {
-  if (err) {
-    console.error('error posting json: ', err)
-    throw err
+function crequest(script, callback){
+  var postData = {
+    script:'printf(\"hello world\")',
+    language:'c',
+    versionIndex:'0',
+    clientId:client,
+    clientSecret:secret
   }
-  var headers = res.headers
-  var statusCode = res.statusCode
-  console.log('headers: ', headers)
-  console.log('statusCode: ', statusCode)
-  console.log('body: ', body)
-})
+
+  var options = {
+    method:'post',
+    url: 'https://api.jdoodle.com/v1/execute',
+    headers: {
+      'Content-Type': 'application/json',
+      'charset' : 'UTF-8'
+    },
+    body: JSON.stringify(postData)
+  };
+
+  request(options, function (err, res, body) {
+    if (err) {
+      console.error('error posting json: ', err)
+      throw err
+    }
+    var headers = res.headers
+    var statusCode = res.statusCode
+    callback(body)
+  })
+}
