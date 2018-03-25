0430caa1ea8412c24eb162199c317a83d5bb0fec
kevinfang2 <kevkev0123@gmail.com>
2018-03-24 22:51:53 -0700

    textfile format lul

--- process.py
+++ process.py
@@ -1,9 +1,11 @@
-with open ('general_func.txt') as f:
+import sys
+infile = sys.argv[1]
+with open (infile) as f:
     same = f.read()
     same = same.replace("\\n", "").replace("\\t", "").replace("\\r", "").replace("\\v", "").replace('\"\\\"', '\"\\\\\"')
     same = same.replace("}", "},")
     same = "[" + same + "]"
 
 
-with open('general_func.txt', 'w') as f:
+with open(infile, 'w') as f:
     f.write(same)
--- process2.py
+++ process2.py
@@ -1,11 +1,29 @@
 import json
+import sys
 
-with open('general_func.txt') as f:
-    # same = f.read()
-    # same = json.dumps([same.strip()])
-    # print (same)
-
+infile = sys.argv[1]
+outfile = sys.argv[2]
+with open(infile) as f:
     same = json.load(f)
-
+    names = []
+    descriptions = []
+    params = []
     for x in same:
-        print (x + "\n")
+        if(x == {}):
+            continue
+        names.append(x["name"])
+        descriptions.append(x["description"])
+        if("syntax" in x):
+            params.append(x["syntax"])
+        else:
+            params.append(" ")
+
+names = list(set(names))
+description = list(set(descriptions))
+
+with open(outfile, 'w') as f:
+    for x in range(0,len(names)):
+        name = names[x]
+        description = descriptions[x]
+        param = params[x]
+        f.write(description[6:] + " <split>" + name + " <split> " + " " + " \n")
